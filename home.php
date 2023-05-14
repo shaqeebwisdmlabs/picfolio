7cf0ac5bf52407910c9041d30aa75cac149ab3b3
Shaqeeb Akhtar <shaqeeb.akhtar@wisdmlabs.com>
2023-05-15 00:45:07 +0530

    add: load a image carousel when clicked on a single image

--- home.php
+++ home.php
@@ -14,21 +14,25 @@
             if ($images) {
                 foreach ($images as $image) {
                     $filename = $image['filename'];
-                    $image_title = $image['image_title']; ?>
+                    $image_title = $image['image_title'];
+            ?>
 
-                    <div class='image'>
-                        <img src='uploads/<?php echo $filename; ?>' alt='<?php echo $image_title; ?>' loading="lazy">
-                        <div class='image-overlay'>
-                            <p class='image-title fw-bold'><?php echo $image_title; ?></p>
-                            <div class='image-actions'>
-                                <button class='btn--action'>
-                                    <img src='./assets/images/share-icon.svg' alt=''>
-                                </button>
-                                <button class='btn--action' onclick="location.href='update.php?id=<?php echo $image['id']; ?>'"><img src='./assets/images/edit-icon.svg' alt=''></button>
-                                <button class='btn--action' onclick="location.href='delete.php?id=<?php echo $image['id']; ?>'"><img src='./assets/images/delete-icon.svg' alt=''></button>
+
+                    <a href="/image.php?id=<?php echo $image['id']; ?>">
+                        <div class='image'>
+                            <img src='uploads/<?php echo $filename; ?>' alt='<?php echo $image_title; ?>' loading="lazy">
+                            <div class='image-overlay'>
+                                <p class='image-title fw-bold'><?php echo $image_title; ?></p>
+                                <div class='image-actions'>
+                                    <button class='btn--action'>
+                                        <img src='./assets/images/share-icon.svg' alt=''>
+                                    </button>
+                                    <button class='btn--action' onclick="location.href='update.php?id=<?php echo $image['id']; ?>'"><img src='./assets/images/edit-icon.svg' alt=''></button>
+                                    <button class='btn--action' onclick="location.href='delete.php?id=<?php echo $image['id']; ?>'"><img src='./assets/images/delete-icon.svg' alt=''></button>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    </a>
             <?php }
             } else {
                 echo "<div><h3 class='fw-bold fs-title'>You haven't uploaded any images yet!</h3></div>";
